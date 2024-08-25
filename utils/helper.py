825b0f657765b8279e7f8bd6b60ad00ff982eb28
slnjke <antimonov342@mail.ru>
2024-08-25 15:47:12 +0400

    fix lint errors

--- utils/helper.py
+++ utils/helper.py
@@ -4,7 +4,6 @@ from allure_commons.types import AttachmentType
 
 
 class Helper:
-    
     def attach_response(self, response):
         response = json.dumps(response, indent=4)
         allure.attach(body=response, name="API Response", attachment_type=AttachmentType.JSON)
